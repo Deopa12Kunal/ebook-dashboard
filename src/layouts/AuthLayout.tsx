dbe79efbe50dd5e0d1994802f38c86b3cd0d962d
kunal <deopakunal@gmail.com>
2024-07-05 12:00:18 +0530

    Make routes protected

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,8 +1,16 @@
 // import React from 'react'
-import { Outlet } from "react-router-dom"
+import { Outlet,Navigate } from "react-router-dom"
+import useTokenStore from '@/store';
+
 
 
 const AuthLayout = () => {
+     const token = useTokenStore(state=>state.token);
+
+  if(token)
+    {
+return<Navigate to ={'/dashboard/home'} replace></Navigate>
+   }
   return (
     <>
    <Outlet/>
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet,Navigate } from "react-router-dom";
 
 import {
   Bell,
@@ -32,10 +32,17 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import useTokenStore from '@/store';
 
 
 const DashboardLayout = () => {
+   const token = useTokenStore(state=>state.token);
+   if(token==="")
+    {
+return<Navigate to ={'/auth/login'} replace></Navigate>
+   }
   return (
+
     <div>
         <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
